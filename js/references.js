a394b6e14966f1af4a7605132a8bad2c5c745eb5
achemete <hmartineztobar@gmail.com>
2017-12-07 00:14:33 +0100

    Path fix
    path fix + jq fix

--- js/references.js
+++ js/references.js
@@ -1,9 +1,9 @@
 $("#contact-btn").click(function(){
 	console.log("click contact from references");
-	$("#main").load("contact.html");
+	$("#main").load("./contact.html");
 });	
 
 $("#explore-algs").click(function(){
 		console.log("click en explore algs");
-		$("#main").load("b-algs.html");
+		$("#main").load("./b-algs.html");
 });
