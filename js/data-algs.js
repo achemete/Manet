71a5527a1aa06a0aaea264c87592f2c55850ab97
achemete <hmartineztobar@gmail.com>
2017-12-06 22:47:25 +0100

    Initial commit

---
+++ js/data-algs.js
@@ -0,0 +1,10 @@
+console.log("he sido cargad");
+
+$(document).ready(function(){	
+	$("#backToCategories").click(function(){
+		console.log("435yutkgjh");
+		$("#main").load("biblio.html");
+	});
+
+    
+})
