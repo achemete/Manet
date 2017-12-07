0d6c4d2b1f7de5d90cf6e2523787b718f193200c
achemete <hmartineztobar@gmail.com>
2017-12-07 15:02:54 +0100

    Important features

---
+++ js/home.js
@@ -0,0 +1,9 @@
+let temp = parseInt($(".circle span").text())
+$('.circle').circleProgress({
+    fill: {gradient: ['red', 'orange']},
+    startAngle: -Math.PI / 2,
+    size: 133
+  }).on('circle-animation-progress', function(event, progress) {
+    $(this).find('span').html(parseInt(progress*temp));
+});
+console.log("hola main");
